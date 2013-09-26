d55a9ff3870e148e74e79b80595b2fa9c05b1184
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-09-26 16:28:24 -0700

    Using mail gem, same one as Rails

--- main.rb
+++ main.rb
@@ -1,7 +1,39 @@
 require "sinatra"
 require "sinatra/reloader"
 
+# Gem for sending mail
+require 'mail'
+
+# JSON parsing gem
+require 'json'
+
+# Gem used for debugging
+require "pry"
+
 # API POST method
 post "/send" do
+
+  # Get data from user
+  mail = JSON.parse(params.keys.first)
+
+  # Make sure there are some default values
+  mail[:to] ||= "frankywahl@gmail.com"
+  mail[:from] ||= "frankywahl@gmail.com"
+  mail[:subject] ||= "A subject"
+  mail[:body] ||= "Body"
+
+  # Create email object
+  email = Mail.new do
+    from     mail[:from]
+    to       mail[:to]
+    subject  mail[:subject]
+    body     mail[:body]
+  end
+
+  # Make sure it uses sendmail
+  email.delivery_method :sendmail
+
+  # Actually send the e-mail
+  email.deliver
 
 end
