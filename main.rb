079c6fa17d468fbac44df0cea2d16473f00a409f
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-09-26 16:13:58 -0700

    First version that needs to run on server with sendmail

--- main.rb
+++ main.rb
@@ -1,13 +1,36 @@
 require "sinatra"
 require "sinatra/reloader"
 
+# JSON parsing gem
+require 'json'
+
 # Gem used for debugging
 require "pry"
 
 # API POST method
 post "/send" do
 
-  binding.pry
+  # Gem to send an email
+  require "pony"
+
+  # Create a mail oject fromm the params sent
+  mail = JSON.parse(params.keys.first)
+
+
+  # Make sure there are some default values
+  mail[:to] ||= "frankywahl@gmail.com"
+  mail[:from] ||= "frankywahl@gmail.com"
+  mail[:subject] ||= "A subject"
+  mail[:body] ||= "Body"
+
+  Pony.mail({
+    to: mail[:to],
+    from: mail[:from],
+    subject: mail[:subject],
+    body: mail[:body]
+  })
+
+
 
 
 end
