513c48331679e578a681234ec44f429af8f06d1a
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-09-26 17:47:32 -0700

    Sending e-mail now but cannot spoof sender

--- main.rb
+++ main.rb
@@ -10,28 +10,29 @@ require 'json'
 # Gem used for debugging
 require "pry"
 
+
+require_relative "config/mailer"
+
 # API POST method
 post "/send" do
 
   # Get data from user
   mail = JSON.parse(params.keys.first)
 
+
   # Make sure there are some default values
-  mail[:to] ||= "Franky W. <frankywahl@gmail.com>"
-  mail[:from] ||= "Somebody <noreply@example.com>"
-  mail[:subject] ||= "A subject"
-  mail[:body] ||= "Body"
+  mail["to"] ||= "Franky W. <frankywahl@gmail.com>"
+  mail["from"] ||= "Somebody <noreply@example.com>"
+  mail["subject"] ||= "A subject"
+  mail["body"] ||= "Body"
 
   # Create email object
   email = Mail.new do
-    from     mail[:from]
-    to       mail[:to]
-    subject  mail[:subject]
-    body     mail[:body]
+    from     mail["from"]
+    to       mail["to"]
+    subject  mail["subject"]
+    body     mail["body"]
   end
-
-  # Make sure it uses sendmail
-  email.delivery_method :sendmail
 
   # Actually send the e-mail
   email.deliver
